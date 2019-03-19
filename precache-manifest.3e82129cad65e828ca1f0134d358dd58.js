25fc69332db17f3d5ab37642d7b6d4dde003ff28
Yauheni Humanau <idracat@gmail.com>
2019-03-19 15:24:36 +0300

    Updates

---
+++ precache-manifest.3e82129cad65e828ca1f0134d358dd58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.e6fc6960.js"
+  },
+  {
+    "revision": "6f128dca0b54a02a7721",
+    "url": "/movie_app/static/js/main.779c184f.chunk.js"
+  },
+  {
+    "revision": "a35ee8e669eeb51c0deb",
+    "url": "/movie_app/static/js/2.639102df.chunk.js"
+  },
+  {
+    "revision": "6f128dca0b54a02a7721",
+    "url": "/movie_app/static/css/main.ccba1876.chunk.css"
+  },
+  {
+    "revision": "a5920c51cfeafc39069d5ceb0a9d410b",
+    "url": "/movie_app/index.html"
+  }
+];
